5160e08ae77e15592e3486fb2fa00ace0f69ff1e
Ashley MacWhirter <macwhirtera@gmail.com>
2017-05-29 16:56:26 -0400

    saving and posting articles

---
+++ server.js
@@ -0,0 +1,161 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
+// Initialize Express
+var app = express();
+
+// Use body parser with our app
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.set('views', __dirname + '/views');
+app.engine("handlebars", exphbs({ defaultLayout: "main", layoutsDir: __dirname + "/views/layouts" }));
+app.set("view engine", "handlebars");
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/nhlscrape");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+//=========Routes==========//
+
+// A GET request to scrape the nhl/oilers website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("https://www.nhl.com/oilers/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("h4.headline-link").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).text();
+      result.link = $(this).parent("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
+
+app.get("/", function (req, res) {
+  Article.find({})
+    .exec(function (error, data) {
+      if (error) {
+        res.send(error);
+      }
+      else {
+        var newsObj = {
+          Article: data
+        };
+        res.render("index", newsObj);
+      }
+    });
+});
+
+app.post("/notes/:id", function (req, res) {
+  var newNote = new Note(req.body);
+  newNote.save(function (error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      console.log("this is the DOC " + doc);
+      Article.findOneAndUpdate({
+        "_id": req.params.id
+      },
+        { $push: { "note": doc._id } }, {new: true},  function (err, doc) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("note saved: " + doc);
+            res.redirect("/notes/" + req.params.id);
+          }
+        });
+    }
+  });
+});
+
+app.get("/notes/:id", function (req, res) {
+  console.log("This is the req.params: " + req.params.id);
+  Article.find({
+    "_id": req.params.id
+  }).populate("note")
+    .exec(function (error, doc) {
+      if (error) {
+        console.log(error);
+      }
+      else {
+        var notesObj = {
+          Article: doc
+        };
+        console.log(notesObj);
+        res.render("notes", notesObj);
+      }
+    });
+});
+
+app.get("/delete/:id", function (req, res) {
+  Note.remove({
+    "_id":req.params.id
+  }).exec(function (error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      console.log("note deleted");
+      res.redirect("/" );
+    }
+  });
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});

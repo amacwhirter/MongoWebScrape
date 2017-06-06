66efde95096273c261e4e434876160f341938592
Ashley MacWhirter <macwhirtera@gmail.com>
2017-06-05 22:46:29 -0400

    save

--- server.js
+++ server.js
@@ -46,10 +46,6 @@ db.once("open", function() {
 });
 
 //=========Routes==========//
-//home page
-app.get("/", function(req, res) {
-    res.render('index');
-});
 
 app.get("/", function (req, res) {
   Article.find({})
